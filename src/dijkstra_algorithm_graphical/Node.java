5e771dc21822a8a112863e76d5a10f65fece6535
Emirhan Yener <yeneremirhann@gmail.com>
2022-04-26 00:53:20 +0300

    neighbor list

--- src/dijkstra_algorithm_graphical/Node.java
+++ src/dijkstra_algorithm_graphical/Node.java
@@ -3,12 +3,35 @@ package dijkstra_algorithm_graphical;
 import javax.swing.JButton;
 
 public class Node extends JButton{
-	Node parentNode;
-	int status;
+	private Node parentNode;
+	private int status;
+	private int xPos;
+	private int yPos;
 	
 	Node(){
 		super();
 		parentNode = null;
 		status = 0;
+	}
+	
+	public void setPos(int x, int y) {
+		this.xPos = x;
+		this.yPos = y;
+	}
+	public void setVisited(Node parent) {
+		this.status = 2;
+		parentNode = parent;
+	}
+	public void setNeighbour() {
+		this.status = 1;
+	}
+	public int getStatus() {
+		return this.status;
+	}
+	public int getX() {
+		return this.xPos;
+	}
+	public int getY() {
+		return this.yPos;
 	}
 }
--- src/dijkstra_algorithm_graphical/Window.java
+++ src/dijkstra_algorithm_graphical/Window.java
@@ -1,27 +1,29 @@
 package dijkstra_algorithm_graphical;
 
 import java.awt.GridLayout;
+import java.util.*;
 
 import javax.swing.*;
 
 public class Window {
 
 	Node[][] nodeArray = new Node[10][10];
+	List<Node> neighborList = new ArrayList<Node>();
 	
 	public Window() {
 		JFrame frame = new JFrame("dijkstra algorithm");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setBounds(50, 50, 500, 500);
 		frame.setLayout(new GridLayout(10,10));
-	
+		
 		for (int i = 0; i < 10; i++) {
 			for (int j = 0; j < 10; j++) {
 				Node node = new Node();
+				node.setPos(j, i);
 				nodeArray[j][i] = node;
 				frame.add(node);
 			}
 		}
-		
 		frame.show();
 	}
 }
