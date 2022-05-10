6a31d0abae4a0d59e8fc9f6c8dcfc5310278b786
Emirhan Yener <yeneremirhann@gmail.com>
2022-05-10 18:46:37 +0300

    table name show

--- src/dijkstra_algorithm_graphical/NeighboursForm.java
+++ src/dijkstra_algorithm_graphical/NeighboursForm.java
@@ -1,16 +1,18 @@
 package dijkstra_algorithm_graphical;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 public class NeighboursForm extends JFrame {
 	public NeighboursForm(int step, String[][] datas) {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setTitle("Step " + step);
-		setBounds(10, 10, 300, 200);
+		setBounds(10, 10, 400, 300);
 		String column[] = {"X Position", "Y Position", "Parent Node"};
 		JTable table = new JTable(datas, column);
 		add(table);
+		add(new JScrollPane(table));
 		show();
 	}
 }
