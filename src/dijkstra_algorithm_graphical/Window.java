48abc7c84bdd76722bffbda4433245af69c105f3
Emirhan Yener <yeneremirhann@gmail.com>
2022-05-14 23:36:24 +0300

    fix keylistener

--- src/dijkstra_algorithm_graphical/Window.java
+++ src/dijkstra_algorithm_graphical/Window.java
@@ -7,12 +7,12 @@ import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
-
+import java.awt.event.KeyListener;
 import java.util.*;
 
 import javax.swing.*;
 
-public class Window{
+public class Window implements KeyListener, ActionListener{
 	private JFrame frame;
 	private JLabel statusLabel;
 	private JLabel startLabel;
@@ -32,6 +32,8 @@ public class Window{
 		frame.setLayout(null);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.setJMenuBar(new WindowMenu());
+		frame.addKeyListener(SingleObject.InstantiateObj().getNodeAction());
+		frame.setFocusable(true);
 		
 		nodeManager = new NodeManagement(NodeMapCreator.CreateNodeMap(xSize, ySize, SingleObject.InstantiateObj().getNodeAction()), xSize, ySize);
 		
@@ -173,5 +175,29 @@ public class Window{
 				+ "\nI : Node info"
 				+ "\nR : Reset all"
 				+ "\nSpace : Next step");
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		System.out.println("key pressed event");
+		SingleObject.InstantiateObj().getNodeAction().SetEvent(e);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("action event");
+		
 	}
 }
